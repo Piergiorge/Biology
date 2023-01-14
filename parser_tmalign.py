f94b2a40e03dca040cd39e166e202be32aa15855
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2023-01-13 21:32:09 -0300

    Update parser_tmalign.py

--- parser_tmalign.py
+++ parser_tmalign.py
@@ -1,3 +1,5 @@
+import re
+
 seq1 = ""
 seq2 = ""
 distance = ""
@@ -6,8 +8,20 @@ alignment = False
 with open("output_tmalign.txt", "r") as f:
     for line in f.readlines():
         line = line.rstrip()
+
+        pattern = re.search('Aligned length=\s+(\d+), RMSD=\s+(\d+\.\d+)', line)
+        if pattern:
+            print('Aligned length= ', pattern.group(1), end='\t')
+            print('RMSD=', pattern.group(2), end='\t')
+
+        tmscore = re.search('TM-score=\s+(\d+\.\d+)', line)
+        if tmscore:
+            print('TM-score=', tmscore.group(1), end='\t')
+
+
         if ' aligned residue pairs of d < 5.0 A' in line:
             alignment = True
+            print('\n')
             continue
         if alignment:
             if any(i.isalpha() for i in line):
