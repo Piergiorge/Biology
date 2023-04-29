35e2b38e9c7c2f6ae8ea2e5b9f9b053affa4cd4f
Rafael M Piergiorge <rafaelpiergiorge@gmail.com>
2023-04-29 10:06:50 -0300

    Update ggarrange.R

--- multi_plot/ggarrange.R
+++ multi_plot/ggarrange.R
@@ -1,27 +1,35 @@
 library(ggplot2)
 library(ggpubr)
+library(gridExtra)
 
-# Creation of the first plot
-p1 <- ggplot(mtcars, aes(x = mpg, y = wt)) + geom_point() + theme_bw() + labs(title = "A)") + theme(plot.title = element_text(hjust = - 0.1))
+# Create plot A
+p1 <- ggplot(mtcars, aes(x = mpg, y = wt)) + geom_point() + theme_bw() + labs(title = "A)") + theme(plot.title = element_text(hjust = - 0.02))
 
-# Creation of the second plot
-p2 <- ggplot(mtcars, aes(x = mpg, y = qsec)) + geom_point() + theme_bw() + labs(title = "B)") + theme(plot.title = element_text(hjust = - 0.1))
-
-# Creation of the third plot
-p3 <- ggplot(mtcars, aes(x = hp, y = qsec)) + 
+# Create plot B
+p2 <- ggplot(mtcars, aes(x = hp, y = qsec)) + 
   geom_point() + 
   geom_smooth(method = "lm", se = FALSE, color = "red") + 
-  labs(x = "Horsepower", y = "Quarter Mile Time") + theme_bw() + labs(title = "D)") + theme(plot.title = element_text(hjust = - 0.1))
+  labs(x = "Horsepower", y = "Quarter Mile Time") + theme_bw() + labs(title = "B)") + theme(plot.title = element_text(hjust = - 0.02))
 
-# Creation of the fourth plot
-p4 <- ggplot(mtcars, aes(x = factor(gear), fill = factor(gear))) +
-  geom_bar() + labs(x = "Number of Cylinders", y = "Count") + theme_bw() + labs(title = "E)") + theme(plot.title = element_text(hjust = - 0.1))
+# Create plot C
+p3 <- ggplot(mtcars, aes(x = factor(gear), fill = factor(gear))) +
+  geom_bar() + labs(x = "Number of Cylinders", y = "Count") + theme_bw() + labs(title = "C)") + theme(plot.title = element_text(hjust = - 0.02))
 
-# Creation of the fifth plot
+# Create plot D
+p4 <- ggplot(mtcars, aes(x = mpg, y = qsec)) + geom_point() + theme_bw() + labs(title = "D)") + theme(plot.title = element_text(hjust = - 0.02))
+
+# Create plot E
 p5 <- ggplot(mtcars, aes(x = factor(vs), y = wt, fill = factor(am))) + 
   geom_boxplot() + 
   labs(x = "Engine Type", y = "Weight") + 
-  scale_fill_discrete(name = "Transmission", labels = c("Automatic", "Manual")) + theme_bw() + labs(title = "C)") + theme(plot.title = element_text(hjust = - 0.1))
+  scale_fill_discrete(name = "Transmission", labels = c("Automatic", "Manual")) + theme_bw() + labs(title = "E)") + theme(plot.title = element_text(hjust = - 0.02))
 
-# Arrange the plots in a panel side by side
-ggarrange(p1, p2, p3, p4, p5, nrow = 3, ncol = 2)
+# Arrange the 5 plots in a 3x2 grid using ggarrange() and export
+svg("output.svg",width=28,height=14)
+  grid.arrange(
+    arrangeGrob(p1, p2, ncol = 2),
+    arrangeGrob(p3, p4, ncol = 2),
+    arrangeGrob(p5, ncol = 2),
+    nrow = 3
+  )
+dev.off()
